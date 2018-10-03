7d54a10bac11e6b5a8703f14dbcdea5fc42c0b4d
vardhit123 <43814816+vardhit123@users.noreply.github.com>
2018-10-03 18:42:46 +0530

    Update vardhitjain.c

--- vardhitjain.c
+++ vardhitjain.c
@@ -4,3 +4,14 @@ int main()
 printf("welcome to c")
 return 0;
 }
+
+******************************************************
+
+#include <stdio.h>
+
+int main()
+{
+    printf("welcomr to c");
+
+    return 0;
+}
