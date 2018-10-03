d315437428d5ca623c763620cfb5db6dfb05038a
vardhit123 <43814816+vardhit123@users.noreply.github.com>
2018-10-03 18:09:41 +0530

    Create vardhit.c

---
+++ vardhit.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n
+printf("entre the number\n");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("the number is even\n");
+}
+else
+{
+printf("the number is odd\n");
+}
+return 0;
+}
