c4428382b2b365a31cef7505e4d3b234f53a7d5e
vardhit123 <43814816+vardhit123@users.noreply.github.com>
2018-10-04 18:33:06 +0530

    Create  intiger.c

---
+++ intiger.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("entre the value for a\n");
+    scanf ("%d",&a);
+    printf("entre the value for b\n");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("the sum of the given intiger is %d",sum);
+    return 0;
+}
+
+
+
